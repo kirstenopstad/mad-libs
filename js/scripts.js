d06b565c86883f3c473cd9a064cfddd27b9bb09d
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-19 11:00:43 -0700

    Add mad lib functionality
    Co-authored-by: Ruben Giosa <rubenalexgiosa@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 // When website loads, run
-window.onload = function(){
+window.onload = function() {
   // When submit happens
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-    // Retrieve input values
-    let name1 = document.getElementById("name1Input").value;
-    let noun1 = document.getElementById("noun1Input").value;
-    let adjective1 = document.getElementById("adjective1Input");
-    
-    // Replace blanks with words
-    document.getElementById("name1");
-    console.log(name1);
-
     // Stop auto reload
     event.preventDefault();
-  }
-}
+
+    // Retrieve input values
+    const name1 = document.getElementById("name1Input").value;
+    const noun1 = document.getElementById("noun1Input").value;
+    const adjective1 = document.getElementById("adjective1Input").value;
+    
+    // Replace blanks with words
+    document.querySelector("span#name1").innerText = name1;
+    document.querySelector("span#noun1").innerText = noun1;
+    document.querySelector("span#adjective1").innerText = adjective1;
+
+    const story = document.querySelector("p#story");
+    story.removeAttribute("class");
+  };
+};
   
